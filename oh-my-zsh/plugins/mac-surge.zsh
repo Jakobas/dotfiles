af5b544b07f8220cb8e0f64fe0553663df224998
Septs <github@septs.pw>
2017-03-31 21:33:09 +0800

    update mac-surge script

--- oh-my-zsh/plugins/mac-surge.zsh
+++ oh-my-zsh/plugins/mac-surge.zsh
@@ -2,18 +2,30 @@ if [[ "$(uname)" != 'Darwin' ]]; then
 	return
 fi
 
-surge-enable() {
+surge() {
 	local -r proxy="http://127.0.0.1:6152"
-	export http_proxy="$proxy"
-	export https_proxy="$proxy"
-}
-
-surge-disable() {
-	unset http_proxy
-	unset https_proxy
-}
-
-surge-status() {
-	echo "HTTP_PROXY : $http_proxy"
-	echo "HTTPS_PROXY: $https_proxy"
+	case "$1" in
+		on)
+			export http_proxy="$proxy"
+			export https_proxy="$proxy"
+			echo "set HTTP_PROXY, HTTPS_PROXY done"
+			;;
+		off)
+			unset http_proxy
+			unset https_proxy
+			echo "unset HTTP_PROXY, HTTPS_PROXY done"
+			;;
+		echo)
+			echo "http_proxy=$http_proxy"
+			echo "https_proxy=$https_proxy"
+			;;
+		*)
+			if [[ "$proxy" == "$http_proxy" ]] && [[ "$proxy" == "$https_proxy" ]]; then
+				echo "status: on\n"
+			else
+				echo "status: off\n"
+			fi
+			echo "usage: $0 {on,off,echo}"
+			;;
+	esac
 }

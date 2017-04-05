528573696fc50d372811596383cd6cd79164627c
Septs <github@septs.pw>
2017-04-05 23:22:20 +0800

    add brew bin path

--- oh-my-zsh/plugins/load-envs.zsh
+++ oh-my-zsh/plugins/load-envs.zsh
@@ -1,5 +1,7 @@
 # shellcheck disable=SC2148,SC1090
-# load dot bin
+# set brew bin path
+[[ -d "/usr/local/sbin" ]] && export PATH="/usr/local/sbin:$PATH"
+# set dot bin path
 [[ -d "$HOME/.bin" ]] && export PATH="$HOME/.bin:$PATH"
 # load node version manager
 [[ -d "$HOME/.nvm" ]] && source "$HOME/.nvm/nvm.sh"
